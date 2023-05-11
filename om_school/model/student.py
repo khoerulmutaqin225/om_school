cfb085035f7687a1a540cce183c28ee1cfe0ee3d
khoerulmutaqinkm <khoerulmutaqin225@gmail.com>
2023-05-11 14:41:15 +0700

    cara menampilkan video preview menggunakan widget baru

--- om_school/model/student.py
+++ om_school/model/student.py
@@ -1,22 +1,35 @@
 # oimp
-from odoo import  fields, models
+from odoo import api,fields, models
+
+# library external video view
+from odoo.addons.website.tools import get_video_embed_code
+
 
 class SchoolStudent(models.Model):
     _name = 'school.student'
-    _inherit =['mail.thread', 'mail.activity.mixin']
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Student Table'
 
     name = fields.Char(string='Name', required=True)
-    age  = fields.Integer(string='Age')
+    age = fields.Integer(string='Age')
     guardian = fields.Char(string='Guardian')
+    customer_id = fields.Many2one('res.partner', string='Customer')
+    supplier_id = fields.Many2one('res.partner', string='Supplier')
+
     note = fields.Text('Note')
     gender = fields.Selection([
         ('male', 'Male'),
         ('female', 'Female'),
         ('other', 'Other'),
-        
+
     ], string='gender')
     image = fields.Binary(string='Image')
     active = fields.Boolean(string='Active', default=True)
+    video_url = fields.Char('Video URL',
+                            help='URL of a video for showcasing your product')
+    embed_code = fields.Char(compute="_compute_embed_code")
     
-    
+    @api.depends('video_url')
+    def _compute_embed_code(self):
+        for rec in self:
+            rec.embed_code = get_video_embed_code(rec.video_url)
