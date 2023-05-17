8c7fa4c0585a260cbf767042fd1fa1454714086c
khoerulmutaqinkm <khoerulmutaqin225@gmail.com>
2023-05-17 16:00:18 +0700

    menambahkan javascript odoo

---
+++ js_framework_sample/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'JS Framework',
+    'version': '13.0.1.0.0',
+    'summary': '',
+    'category': 'Extra Tools',
+    'author': 'Khoerul Mutaqin',
+    'maintainer': '',
+    'website': 'khoerulmutaqinkm.github.io',
+    'license': '',
+    'contributors': [
+        '',
+    ],
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/templates.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ js_framework_sample/static/src/js/framework_sample.js
@@ -0,0 +1,7 @@
+// alert("Loaded !!")
+// console.log("loaded again")
+odoo.define("js_framework_sample.SampleFramework", function(required){
+    "use strict";
+
+    var rpc = require("web.rpc");
+});
